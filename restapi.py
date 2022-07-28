ff2df609a8efdb179099b4cf28600983135b6ac8
Darshan02-S <76936006+Darshan02-S@users.noreply.github.com>
2022-07-28 17:24:02 +0530

    Add files via upload

--- restapi.py
+++ restapi.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 courses = [{'name': 'python programming',
-            'id': '1',
+            'id': 1,
             'desc': 'valuable course'},
            {'name': 'java programming',
-            'id': '2',
+            'id': 2,
             'desc': 'impressive course'},
            {'name': 'c programming',
-            'id': '3',
+            'id': 3,
             'desc': 'aggressive course'},
            {'name': 'c++ programming',
-            'id': '4',
+            'id': 4,
             'desc': 'awesome course'}
            ]
 
@@ -54,29 +54,37 @@ def get_by_course_name(name):
 #the command used in 2nd terminal is "curl -i -H "Content-type:Application/json" -X POST http://localhost:5000/courses"
 #where -i => status code, -H => header format, "Content-type:Application/json" => return type format, -X POST => http method and thn url
 def post_course():
-    course = {'name': 'java script programming',
-            'id': '5',
-            'desc': 'awesome course'}
-    courses.append(course)
-    return jsonify({'Created': course})
+    course = request.get_json()
+    data = {
+        'name': course['name'],
+        'id': course['id'],
+        'desc': course['desc']
+    }
+    courses.append(data)
+    return jsonify({'Created': data})
 
 @app.route("/courses/<int:id>", methods=["PUT"])
 #here when you specifiy "id" you should provide it in form of index number
 #the command used in 2nd terminal is "curl -i -H "Content-type:Application/json" -X PUT http://localhost:5000/courses/"id_number(index number)""
 #where -i => status code, -H => header format, "Content-type:Application/json" => return type format, -X POST => http method and thn url
 def put_value(id):
-    courses[id]['desc'] = "luxurious course"
-    return jsonify({'Courses': courses[id]})
+    id = request.get_json()
+    try:
+        courses[(id["id"])-1]['desc'] = id["desc"]
+        courses[(id["id"])-1]['name'] = id["name"]
+        courses[(id["id"])-1]['id'] = id["id"]
+        return jsonify({'Courses': courses[(id["id"])-1]})
+    except:
+        return "course not found"
 
 @app.route("/courses/<int:id>", methods=["DELETE"])
-#here when you specifiy "id" you should provide it in form of index number
-#the command used in 2nd terminal is "curl -i -H "Content-type:Application/json" -X DELETE http://localhost:5000/courses/"id_number(index number)""
-#where -i => status code, -H => header format, "Content-type:Application/json" => return type format, -X POST => http method and thn url
 def delete_course(id):
-    courses.remove(courses[id])
-    return jsonify({"result": True})
-
-
+    id = request.get_json()
+    try:
+        courses.remove(courses[(id['id'])-1])
+        return jsonify({"result": True})
+    except:
+        return "course id not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
